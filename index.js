727202fc974c22c6b5f824ce5367dedf07611adc
MD Limon Farazi <abidislamabir@gmail.com>
2022-06-04 21:57:50 +0600

    get single api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -27,6 +27,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result)
     });
+    // get and read single api
+    app.get("/item/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : ObjectId(id)};
+      const result = await collectionDatabase.findOne(query);
+      res.send(result);
+    });
     // Create api
     app.post('/item', async(req,res)=>{
       const query = req.body;
@@ -34,6 +41,7 @@ async function run() {
       res.send(result);
     })
     // UPDATE api
+    
     // DELETE API
   } finally {
     // await client.close();
