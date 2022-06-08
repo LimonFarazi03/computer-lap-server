7f5ee5f5b9628c02f7de833f5c2bbe70fe99b5d1
MD Limon Farazi <abidislamabir@gmail.com>
2022-06-08 20:37:38 +0600

    delete api added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const { MongoClient, ServerApiVersion,ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
@@ -21,41 +21,41 @@ async function run() {
     await client.connect();
     const collectionDatabase = client.db("items").collection("item");
     // get and read all api
-    app.get("/items", async(req, res) => {
+    app.get("/items", async (req, res) => {
       const query = req.body;
       const cursor = collectionDatabase.find(query);
       const result = await cursor.toArray();
-      res.send(result)
+      res.send(result);
     });
     // get and read single api
-    app.get("/item/:id", async(req, res) => {
+    app.get("/item/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : ObjectId(id)};
+      const query = { _id: ObjectId(id) };
       const result = await collectionDatabase.findOne(query);
       res.send(result);
     });
     // Create api
-    app.post('/item', async(req,res)=>{
+    app.post("/item", async (req, res) => {
       const query = req.body;
       const result = await collectionDatabase.insertOne(query);
       res.send(result);
-    })
+    });
     // UPDATE api
-    
+
     // DELETE API
     app.delete("/item/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id) };
       const result = await collectionDatabase.deleteOne(filter);
-    
+
       if (result.deletedCount === 1) {
         console.log("Successfully deleted one document.", id);
       } else {
         console.log("No documents matched the query. Deleted 0 documents.", id);
       }
       res.send(result);
-    })}
-   finally {
+    });
+  } finally {
     // await client.close();
   }
 }
